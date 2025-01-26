5885c28bdd81c616dedfa569ec6bc4defccb9ea6
Ahmad Ihsan <ihsanashi@gmail.com>
2025-01-26 10:48:25 +0800

    feat: add Supabase auth middleware, user secrets, and scalar

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Scalar.AspNetCore;
+using server.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,11 +9,23 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Load user secrets
+builder.Configuration.AddUserSecrets<Program>();
+
 var app = builder.Build();
+
+app.MapWhen(
+    context => context.Request.Path.StartsWithSegments("/api/v1"),
+    appBuilder =>
+    {
+        appBuilder.UseSupabaseAuth();
+    }
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapScalarApiReference(); // scalar/v1
     app.MapOpenApi();
 }
 
